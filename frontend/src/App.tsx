72bcaf7c700812cca9e3d8975d63424d9096d9c0
pierrelisandrumartin <pierrelisandrumartin@gmail.com>
2026-05-12 17:14:14 +0200

    feat(frontend): add login page with protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
-    <div className="flex bg-[#0F1117] min-h-screen">
-      <Navbar />
-      <main className="ml-60 flex-1 p-8">
-        <Home />
-      </main>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <div className="flex bg-[#0F1117] min-h-screen">
+                  <Navbar />
+                  <main className="ml-60 flex-1 p-8">
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                    </Routes>
+                  </main>
+                </div>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
